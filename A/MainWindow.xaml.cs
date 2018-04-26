72793b2610d2f1ab34e32772432cefead9102a72
m305-29\w401 <egg880604@gmail.com>
2018-04-26 18:24:23 +0800

    第二次作業

--- A/MainWindow.xaml.cs
+++ A/MainWindow.xaml.cs
@@ -27,7 +27,58 @@ namespace A
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+
             this.Close();
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            HeightBox.Text = "ABCDEFG";
+            HeightBox.Background = Brushes.Yellow;
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button3_Click(object sender, RoutedEventArgs e)
+        {
+            double w = double.Parse(WeightBox.Text);
+            double h = double.Parse(HeightBox.Text) / 100;
+            double bmi = w / (h * h);
+
+            Answer.Text = bmi.ToString();
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            HeightBox.Text = HeightBar.Value.ToString();
+        }
+
+        private void HeightBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            HeightBox.Text = HeightBar.Value.ToString();
+
+            double w = double.Parse(WeightBox.Text);
+            double h = double.Parse(HeightBox.Text) / 100;
+            double bmi = w / (h * h);
+
+            Answer.Text = bmi.ToString();
+        }
+
+        private void WeighBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (IsInitialized)
+            {
+                WeightBox.Text = WeightBar.Value.ToString();
+
+                double w = double.Parse(WeightBox.Text);
+                double h = double.Parse(HeightBox.Text) / 100;
+                double bmi = w / (h * h);
+
+                Answer.Text = bmi.ToString();
+            }
         }
     }
 }
